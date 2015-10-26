9d02526d9dfcea3b11629c3685079aed4f5638b0
David Correa Orozco <davidcroz1@gmail.com>
2015-10-25 20:08:01 -0700

    added pins and learned how to use scaffolds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151025073035) do
+ActiveRecord::Schema.define(version: 20151025224807) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
